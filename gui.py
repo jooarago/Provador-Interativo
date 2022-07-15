291495aa6532d82074843015c655c51832588c7f
jooaragao <joao.sales.aragao@gmail.com>
2022-07-15 15:50:37 -0300

    adicionei messagebox

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
+from os import popen
 from pyswip import Prolog
 from pyswip import Functor
 from tkinter import *
+from tkinter import messagebox
 
 prolog = Prolog()
 prolog.consult("provador.pl")
@@ -52,30 +54,41 @@ e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0)
 
 #MOSTRA A LINHA DE DEDUCAO DA ENTRADA QUANDO CLICADO
-def buttonclick():
-    
+def button1click():
+    global pop 
+    pop = Toplevel(root)
+    pop.geometry("250x150")
+    pop.title("Satisfatibilidade de "+e.get())
+
     list(prolog.query("reiniciar()"))
     list(prolog.query("translate("+e.get()+")"))
     list(prolog.query("resolucao()"))
-    
-    
+
+    #DIZER SE A FÓRMULA É SATISFATÍVEL OU NÃO
+    i = prolog.query("clause([])")
+    if(format_result(i)=="true."):
+        label = Label(pop, text=e.get()+" é insatisfatível, pois do conjunto de premissas foi deduzida a cláusula vazia.")
+        label.grid()
+    else:
+        label = Label(pop, text=e.get()+" é satisfatível, pois do conjunto de premissas não foi deduzida a cláusula vazia.")
+        label.grid()
+
+
     #MOSTRAR PREMISSAS
     for i in prolog.query("clause(R), not(res(_,_,R)), clausenum(R, NumR)"):
-        label = Label(root, text=format_value(i['NumR'])+format_value(i['R']))
+        label = Label(pop, text=format_value(i['NumR'])+" "+format_value(i['R'])+" PREMISSA")
         label.grid()
     
     #MOSTRAR O RESTO DA DEDUCAO
     for i in prolog.query("clause(R), res(X,Y,R), clausenum(X, NumX), clausenum(Y, NumY), clausenum(R, NumR)"):
         
-        string = format_value(i['NumR'])+format_value(i['R'])+' RES '+format_value(i['NumX'])+' '+format_value(i['NumY'])
+        string = format_value(i['NumR'])+" "+format_value(i['R'])+' RES '+format_value(i['NumX'])+' '+format_value(i['NumY'])
         
-        label = Label(root, text=string)
+        label = Label(pop, text=string)
         label.grid()
-
-
    
-button = Button(root, text="Checar satisfatibilidade", command=buttonclick)
+button1 = Button(root, text="Checar satisfatibilidade", command=button1click)
 
-button.grid()
+button1.grid()
 
 root.mainloop()
